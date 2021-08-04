784ed79c7b1a69282682c58508d443408a745265
Abir <abir.pal899@gmail.com>
2021-08-04 23:12:57 +0530

    Update WebManifest and Fix Border Color

--- src/components/slides/postmanstudentsummit/index.tsx
+++ src/components/slides/postmanstudentsummit/index.tsx
@@ -9,13 +9,11 @@ const CanvasDesign = () => {
                 paddingTop: "56.2500%",
                 paddingBottom: "48px",
                 boxShadow: "0 2px 8px 0 rgba(63, 69, 81, 0.16)",
-
                 marginBottom: "0.9em",
                 overflow: "hidden",
-                borderRadius: "8px",
                 willChange: "transform",
             }}>
-            < iframe loading="lazy" style={{ position: "absolute", width: width, height: height, top: 0, left: 0, border: "none", padding: 0, margin: 0 }}
+            < iframe loading="lazy" style={{ position: "absolute", width: width, height: height, top: 0, left: 0, border: "10px solid #FF914D", padding: 0, margin: 0 }}
                 src="https://www.canva.com/design/DAEmJOe-5O0/view?embed" >
             </iframe>
         </div>
